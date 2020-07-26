21572385c066eec0e4030269c7eb02fd8092a753
LEE WEI HAO JONATHAN <leeweihao1996@outlook.com>
2020-07-26 22:14:19 +0800

    Added part 1d

--- part1/src/index.js
+++ part1/src/index.js
@@ -14,6 +14,22 @@ const Hello = ({ name, age }) => {
   );
 };
 
+const History = ({ allClicks }) => {
+  if (allClicks.length === 0) {
+    return (
+      <>
+        <p>No history</p>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <p>All clicks history: {allClicks.join(" ")}</p>
+    </>
+  );
+};
+
 const Button = ({ handleClick, text }) => {
   return (
     <>
@@ -22,14 +38,42 @@ const Button = ({ handleClick, text }) => {
   );
 };
 
-const Display = ({ counter }) => {
-  return <p>The counter: {counter}</p>;
+const Display = ({ counter, clicks, allClicks }) => {
+  return (
+    <>
+      <p>The counter: {counter}</p>
+      <p>Left clicks: {clicks.left}</p>
+      <p>Right clicks: {clicks.right}</p>
+      <History allClicks={allClicks} />
+    </>
+  );
 };
 
 const App = () => {
   const name = "Peter";
   const age = 10;
   const [counter, setCounter] = useState(0);
+  const [clicks, setClicks] = useState({
+    left: 0,
+    right: 0,
+  });
+  const [allClicks, setAll] = useState([]);
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"));
+    setClicks({
+      ...clicks,
+      left: clicks.left + 1,
+    });
+  };
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"));
+    setClicks({
+      ...clicks,
+      right: clicks.right + 1,
+    });
+  };
 
   const incrementCounterByOne = () => {
     setCounter(counter + 1);
@@ -40,15 +84,24 @@ const App = () => {
   const resetCounter = () => {
     setCounter(0);
   };
+
   return (
     <div>
       <h1>Greetings</h1>
       <Hello name="Maya" age={26 + 10} />
       <Hello name={name} age={age} />
-      <Display counter={counter} />
+      <Display counter={counter} clicks={clicks} allClicks={allClicks} />
       <Button handleClick={incrementCounterByOne} text="Increment counter" />
       <Button handleClick={decrementCounterByOne} text="decrement counter" />
       <Button handleClick={resetCounter} text="Reset counter" />
+      <Button
+        handleClick={handleLeftClick}
+        text="Increment Left click counter"
+      />
+      <Button
+        handleClick={handleRightClick}
+        text="Increment Right click counter"
+      />
     </div>
   );
 };
