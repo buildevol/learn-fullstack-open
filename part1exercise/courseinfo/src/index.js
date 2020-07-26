309d5e6aea1e8b9ee4beb860563078bd1c5dadd6
LEE WEI HAO JONATHAN <leeweihao1996@outlook.com>
2020-07-26 20:34:10 +0800

    Added part 1 exercise 1.4

--- part1exercise/courseinfo/src/index.js
+++ part1exercise/courseinfo/src/index.js
@@ -14,7 +14,7 @@ const Part = (props) => {
   return (
     <>
       <p>
-        {props.part} {props.numOfExercises}
+        {props.part.name} {props.part.exercises}
       </p>
     </>
   );
@@ -23,9 +23,9 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <>
-      <Part part={props.parts[0]} numOfExercises={props.numOfExercises[0]} />
-      <Part part={props.parts[1]} numOfExercises={props.numOfExercises[1]} />
-      <Part part={props.parts[2]} numOfExercises={props.numOfExercises[2]} />
+      {props.parts.map((part) => (
+        <Part part={part} />
+      ))}
     </>
   );
 };
@@ -33,28 +33,36 @@ const Content = (props) => {
 const Total = (props) => {
   return (
     <>
-      <p>Number of exercises {props.total}</p>
+      <p>
+        Number of exercises{" "}
+        {props.parts.reduce((accumulator, currentValue) => {
+          return accumulator + currentValue.exercises;
+        }, 0)}
+      </p>
     </>
   );
 };
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
 
   return (
     <div>
       <Header course={course} />
-      <Content
-        parts={[part1, part2, part3]}
-        numOfExercises={[exercises1, exercises2, exercises3]}
-      />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Content parts={[part1, part2, part3]} />
+      <Total parts={[part1, part2, part3]} />
     </div>
   );
 };
