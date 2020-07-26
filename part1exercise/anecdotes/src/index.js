0154fb793dad9d8c0b560181eaa55b63a1a45560
LEE WEI HAO JONATHAN <leeweihao1996@outlook.com>
2020-07-27 00:26:48 +0800

    Added part 1 exercise 1.12

--- part1exercise/anecdotes/src/index.js
+++ part1exercise/anecdotes/src/index.js
@@ -11,29 +11,61 @@ function getRandomIntExclusive(min, max) {
 const Button = ({ handleClick, text }) => {
   return (
     <>
-      <button onClick={handleClick}>next anecdote</button>
+      <button onClick={handleClick}>{text}</button>
     </>
   );
 };
 
 const anecdotes = [
-  "If it hurts, do it more often",
-  "Adding manpower to a late software project makes it later!",
-  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-  "Premature optimization is the root of all evil.",
-  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+  {
+    anecdote: "If it hurts, do it more often",
+    vote: 0,
+  },
+  {
+    anecdote: "Adding manpower to a late software project makes it later!",
+    vote: 0,
+  },
+  {
+    anecdote:
+      "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    vote: 0,
+  },
+  {
+    anecdote:
+      "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    vote: 0,
+  },
+  {
+    anecdote: "Premature optimization is the root of all evil.",
+    vote: 0,
+  },
+  {
+    anecdote:
+      "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    vote: 0,
+  },
 ];
 
 const App = (props) => {
   const [selected, setSelected] = useState(0);
+  const [anecdotes, setAnecdotes] = useState([...props.anecdotes]);
   const handleRandomSelection = () => {
-    setSelected(getRandomIntExclusive(0, props.anecdotes.length));
+    setSelected(getRandomIntExclusive(0, anecdotes.length));
+  };
+  const handleVote = () => {
+    const copy = [...anecdotes];
+    copy[selected] = {
+      anecdote: anecdotes[selected].anecdote,
+      vote: anecdotes[selected].vote + 1,
+    };
+    setAnecdotes(copy);
   };
   return (
     <div>
-      <p>{props.anecdotes[selected]}</p>
-      <Button handleClick={handleRandomSelection} />
+      <p>{anecdotes[selected].anecdote}</p>
+      <p>has {anecdotes[selected].vote} votes</p>
+      <Button handleClick={handleVote} text="vote" />
+      <Button handleClick={handleRandomSelection} text="next anecdote" />
     </div>
   );
 };
