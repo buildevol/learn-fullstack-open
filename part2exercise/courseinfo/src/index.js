7ee6ef2eb3d32809db7e748ba934e40a92ac16d6
LEE WEI HAO JONATHAN <leeweihao1996@outlook.com>
2020-07-31 20:04:58 +0800

    Added my part 2a exercise 2.5 solution

--- part2exercise/courseinfo/src/index.js
+++ part2exercise/courseinfo/src/index.js
@@ -1,58 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Course from "./components/Course";
 import "./index.css";
-
-const Header = (props) => {
-  return (
-    <>
-      <h2>{props.course}</h2>
-    </>
-  );
-};
-
-const Part = (props) => {
-  return (
-    <>
-      <p>
-        {props.part.name} {props.part.exercises}
-      </p>
-    </>
-  );
-};
-
-const Content = (props) => {
-  return (
-    <>
-      {props.parts.map((part) => (
-        <Part key={part.id} part={part} />
-      ))}
-    </>
-  );
-};
-
-const Total = (props) => {
-  // Already coded with reduce method in 2.2 which is also part of 2.3 solution
-  return (
-    <>
-      <h3>
-        {`total of
-        ${props.parts.reduce((accumulator, currentValue) => {
-          return accumulator + currentValue.exercises;
-        }, 0)} exercises`}
-      </h3>
-    </>
-  );
-};
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
-};
 
 const App = () => {
   const courses = [
