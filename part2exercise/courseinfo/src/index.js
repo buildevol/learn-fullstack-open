13a92cee9a98fd220d9d0db4ecb5db23ac5e2f30
LEE WEI HAO JONATHAN <leeweihao1996@outlook.com>
2020-07-31 19:52:27 +0800

    Added my part 2a exercise 2.1 solution

---
+++ part2exercise/courseinfo/src/index.js
@@ -0,0 +1,87 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <>
+      <p>
+        {props.part.name} {props.part.exercises}
+      </p>
+    </>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <>
+      {props.parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+    </>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <>
+      <p>
+        Number of exercises{" "}
+        {props.parts.reduce((accumulator, currentValue) => {
+          return accumulator + currentValue.exercises;
+        }, 0)}
+      </p>
+    </>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
