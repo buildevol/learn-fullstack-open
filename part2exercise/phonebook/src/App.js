a96f4482d0cd575364abb06d79937307c3d29a18
LEE WEI HAO JONATHAN <leeweihao1996@outlook.com>
2020-08-02 00:01:38 +0800

    Added my part 2d exercise 2.15 solution

--- part2exercise/phonebook/src/App.js
+++ part2exercise/phonebook/src/App.js
@@ -76,7 +76,11 @@ function App() {
     if (filteredPersons.length > 0) {
       alert(`${newName} is already added to phonebook`);
     } else {
-      setPersons(persons.concat(personObject));
+      axios
+        .post("http://localhost:3001/persons", personObject)
+        .then((response) => {
+          setPersons(persons.concat(response.data));
+        });
     }
     setNewName("");
     setNewNumber("");
