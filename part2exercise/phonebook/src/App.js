6d01e029a241f743d42152e308191e3cd8b4e8b9
LEE WEI HAO JONATHAN <leeweihao1996@outlook.com>
2020-08-01 12:22:02 +0800

    Added my part 2c exercise 2.11 solution

--- part2exercise/phonebook/src/App.js
+++ part2exercise/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Person = ({ person }) => {
   return (
@@ -51,15 +52,17 @@ const Persons = ({ personsToShow }) => {
 };
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filterInput, setFilterInput] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      const persons = response.data;
+      setPersons(persons);
+    });
+  }, []);
 
   const handleSubmit = (event) => {
     event.preventDefault();
