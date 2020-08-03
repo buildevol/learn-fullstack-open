2f43e6dca8653b2d853c003c7d811bafc5f6f621
LEE WEI HAO JONATHAN <leeweihao1996@outlook.com>
2020-08-03 12:42:18 +0800

    Added part 3b lecture

--- part3/index.js
+++ part3/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const requestLogger = (req, res, next) => {
---
+++ part3frontend/src/App.js
@@ -0,0 +1,118 @@
+import React, { useState, useEffect } from "react";
+import Note from "./components/Note";
+import noteService from "./services/notes";
+
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null;
+  }
+
+  return <div className="error">{message}</div>;
+};
+
+const Footer = () => {
+  const footerStyle = {
+    color: "green",
+    fontStyle: "italic",
+    fontSize: 16,
+  };
+
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, My notes</em>
+    </div>
+  );
+};
+const App = (props) => {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("A new note");
+  const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("Some error happened");
+
+  const hook = () => {
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes);
+    });
+  };
+
+  useEffect(hook, []); // The second paramter is an empty array which means the effect is only run along the first render of the component
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+    };
+
+    noteService.create(noteObject).then((returnedNote) => {
+      setNotes(notes.concat(returnedNote));
+      setNewNote("");
+    });
+  };
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value);
+  };
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter((note) => {
+        return note.important;
+      });
+
+  const handleShowAll = (event) => {
+    setShowAll(!showAll);
+  };
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find((note) => note.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    noteService
+      .update(id, changedNote)
+      .then((returnedNote) => {
+        setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
+      })
+      .catch((error) => {
+        setErrorMessage(
+          `Note ${note.content} was already removed from the server`
+        );
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
+      });
+  };
+  return (
+    <div>
+      <h1>Notes</h1>
+      <Notification message={errorMessage} />
+      <div>
+        <button onClick={handleShowAll}>
+          Show {showAll ? "important" : "all"}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map((note) => {
+          return (
+            <Note
+              key={note.id}
+              note={note}
+              toggleImportance={() => {
+                toggleImportanceOf(note.id);
+              }}
+            />
+          );
+        })}
+      </ul>
+      <form onSubmit={addNote}>
+        <input type="text" value={newNote} onChange={handleNoteChange} />
+        <button type="submit">Save</button>
+      </form>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
