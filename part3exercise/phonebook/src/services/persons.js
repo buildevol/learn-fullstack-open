02af452a6e376879c1965f492482d7506db8e821
LEE WEI HAO JONATHAN <leeweihao1996@outlook.com>
2020-08-04 21:10:40 +0800

    Added my part 3b exercise 3.9 solution

---
+++ part3exercise/phonebook/src/services/persons.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const baseUrl = "http://localhost:3001/api/persons";
+
+const getAll = () => {
+  return axios.get(baseUrl).then((response) => response.data);
+};
+
+const createPerson = (newObject) => {
+  return axios.post(baseUrl, newObject).then((response) => response.data);
+};
+
+const deletePerson = (id) => {
+  return axios.delete(`${baseUrl}/${id}`); // response.data will return empty
+};
+
+const updatePerson = (id, newObject) => {
+  return axios
+    .put(`${baseUrl}/${id}`, newObject)
+    .then((response) => response.data);
+};
+
+export default {
+  getAll,
+  createPerson,
+  deletePerson,
+  updatePerson,
+};
