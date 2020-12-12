25e103badcd0f946ab57b55f4e651abc5680be2b
JonathanLeeWH <leeweihao1996@outlook.com>
2020-12-12 22:15:34 +0800

    Added part 6a lecture redux notes and pure functions immutable

---
+++ part6/simplified-notes-app-redux/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App({ store }) {
+  return (
+    <div>
+      <ul>
+        {store.getState().map((note) => (
+          <li key={note.id}>
+            {note.content} <strong>{note.important ? 'important' : ''}</strong>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
