c461c29e1693e63042d6fb6fed62570479bdf797
JonathanLeeWH <leeweihao1996@outlook.com>
2021-07-12 15:21:10 +0800

    Added my part 9b exercise 9.2 solution

---
+++ part9bexercise/exerciseCalculator.ts
@@ -0,0 +1,75 @@
+import assert from 'assert';
+
+interface Result {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: 1 | 2 | 3;
+  ratingDescription: String;
+  target: number;
+  average: number;
+}
+
+const calculateExercises = (
+  dailyExerciseHoursArr: Array<number>,
+  target: number,
+): Result => {
+  let periodLength = dailyExerciseHoursArr.length;
+  let trainingDays: number = 0,
+    success: boolean,
+    rating: 1 | 2 | 3,
+    ratingDescription: string,
+    average: number,
+    total: number = 0;
+  // The calculation
+  for (let i = 0; i < dailyExerciseHoursArr.length; i++) {
+    if (dailyExerciseHoursArr[i] === 0) {
+      continue;
+    }
+    trainingDays++;
+    total += dailyExerciseHoursArr[i];
+  }
+
+  average = total / periodLength;
+  if (average >= target) {
+    success = true;
+    rating = 3;
+  } else {
+    success = false;
+    const difference: number = average - target;
+
+    if (difference >= 0) {
+      rating = 3;
+      ratingDescription = 'Good Job';
+    } else if (Math.abs(difference) / target <= 0.5) {
+      rating = 2;
+      ratingDescription = 'not too bad but could be better';
+    } else {
+      rating = 1;
+      ratingDescription =
+        'Please do your best to achieve target as you are currently more than 50% away from target';
+    }
+  }
+  return {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average,
+  };
+};
+
+const result: Result = calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2);
+console.log(result);
+
+assert.deepStrictEqual(result, {
+  periodLength: 7,
+  trainingDays: 5,
+  success: false,
+  rating: 2,
+  ratingDescription: 'not too bad but could be better',
+  target: 2,
+  average: 1.9285714285714286,
+});
