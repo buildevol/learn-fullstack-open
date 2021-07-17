7d196b5cb1dbba6d964cdd43ccbc5e6c07ea0f79
JonathanLeeWH <leeweihao1996@outlook.com>
2021-07-17 22:55:54 +0800

    Added my Part 9c exercise 9.9 solution

--- part9cexercise/index.ts
+++ part9cexercise/index.ts
@@ -1,12 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
+
+console.log(PORT);
 
 const app = express();
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
 app.use(express.json());
 
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
   console.log('Ping Received.');
   res.send('pong');
 });
